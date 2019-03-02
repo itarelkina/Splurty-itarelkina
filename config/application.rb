7e0ea578177e5d8ecc7f4d86ac67fb100627e305
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-02 02:00:01 +0000

    Refined the font styles

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,10 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 
 module Splurty
   class Application < Rails::Application
@@ -15,5 +15,13 @@ module Splurty
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
+
   end
 end
