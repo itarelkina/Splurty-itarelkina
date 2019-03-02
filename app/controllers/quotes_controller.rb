309619189c92d478a405848905df6758e230e238
Irina Tarelkina <itarelkina@yahoo.ca>
2019-03-02 19:50:56 +0000

    Added Validate User Data function

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -7,10 +7,13 @@ class QuotesController < ApplicationController
     @quote = Quote.new
   end
 
-  def create
-    Quote.create(quote_params)
-    redirect_to root_path
+def create
+  @quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
+  redirect_to root_path
+end
 
   private
 
